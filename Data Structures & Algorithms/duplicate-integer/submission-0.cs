3e76054c3a9ef403fb391a7abc35703d624120f2
Ashutosh Srivastava <ashutosh.srivastava93@hotmail.com>
2026-04-21 13:51:15 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+        
+        foreach(var item in nums)
+        {
+            if(freq.ContainsKey(item))
+            {
+                freq[item]++;
+            }
+            else
+            {
+                freq[item] = 1;
+            }
+        }
+
+        foreach(var item in freq)
+        {
+            if(item.Value > 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
